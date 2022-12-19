c2dccbabdd05142e5250e696770555cacb78a333
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2022-12-19 14:09:29 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// виборка елементів розмітки
+const gelleryBox = document.querySelector('.gallery');
+const galleryMarkup = createMarkupGallery(galleryItems);
+
+gelleryBox.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// створення lightbox
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+// сет функцій
+
+function createMarkupGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      const markupItemGallery = `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>`;
+      return markupItemGallery;
+    })
+    .join('');
+}
