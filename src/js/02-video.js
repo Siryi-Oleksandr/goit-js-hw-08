9ec28373a98b23692e11122c498d63adee160349
Siryi-Oleksandr <siryyoleksandr@gmail.com>
2022-12-20 12:59:19 +0200

    have done task #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,20 @@
 import Player from '@vimeo/player';
+import { throttle } from 'throttle-debounce';
 
-console.log(Player);
-console.log(1);
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const savedTimeLocalStorage = +localStorage.getItem('videoplayer-current-time');
+
+if (savedTimeLocalStorage) {
+  player.setCurrentTime(savedTimeLocalStorage);
+}
+
+player.on('timeupdate', throttle(1000, onPlay));
+
+function onPlay(data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}
